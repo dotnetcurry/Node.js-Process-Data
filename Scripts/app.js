e86ab0ba857330851101b94c025a5642bdc5ea13
DotNetCurry <dotnetcurry@a2zknowledgevisuals.com>
2015-12-02 17:31:53 +0530

    code

---
+++ Scripts/app.js
@@ -0,0 +1,29 @@
+﻿//1.
+var http = require('http');
+var fs = require('fs');
+
+//2.
+var productPage = fs.readFileSync('../AppPages/MyPage.html');
+
+//3.
+var server = http.createServer(function (req, resp) {
+    //4.
+    if (req.method === "GET") {
+        resp.writeHead(200, { 'content-type': 'text/html' });
+        resp.end(productPage);
+    }
+    //5.
+    if (req.method === "POST") {
+        var productData = '';
+        //6.
+        req.on('data', function (prd) {
+            productData += prd;
+        }).on('end', function () {
+            console.log('The received data is ' + productData.toString());
+            resp.end('Data received  from you is ' + productData.toString());
+        });          
+    }
+});
+server.listen(5050);
+console.log('Server started on  5050');
+
